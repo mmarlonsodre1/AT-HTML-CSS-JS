e47ee20697594880935f1b379a100a042b25d8e1
mmarlonsodre1 <marlon.sodre@al.infnet.edu.br>
2019-12-08 18:26:37 -0300

    Finalizacao do JS + Perguntas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function (){
+$(document).ready(function (){
 var imagens = [
     'img/facebook.png',
     'img/android.png',
@@ -10,88 +10,112 @@ var imagens = [
     'img/windows.png',
     'img/cross.png'
 ];
-
-    const tabuleiro = document.querySelector("#tabuleiro");
+    const board = document.querySelector("#board");
     btnStart = document.querySelector("#btnStart");
     let cardsGame = [];
     let cardHTML = "";
-    let blockGame = true;
-    
+
     imagens.forEach((img) => {
         if (img != 'img/cross.png')
         cardsGame.push(img);
     })
 
+    function menuGame(){
+        for (let index = 0; index < 16; index++) {
+            cardHTML += `<div class="cardGame" data-card="${imagens[8]}">
+                            <img class="up" src="../${imagens[8]}"/>
+                            <img class="down" src="../${imagens[8]}">
+                        </div>`;
+        }
+    }
+
+    menuGame();
+    board.innerHTML = cardHTML;
+
     function sortCards(){
         cardsGame.sort(function( ){return 0.3 - Math.random()});
         cardsGame.forEach((img) => {
-                cardHTML += `<div class="memory-card" data-card="${img}">
-                    <img class="front-face" src="../${img}"/>
-                    <img class="back-face" src="../${imagens[8]}">
-                </div>`;
+                cardHTML += `<div class="cardGame" data-card="${img}">
+                                <img class="up" src="../${img}"/>
+                                <img class="down" src="../${imagens[8]}">
+                            </div>`;
         });
     }
-
-    sortCards();
-    sortCards();
-    tabuleiro.innerHTML = cardHTML;
-
-    function blocking() {
-        blockGame = false;
-        console.log(blockGame);
+ 
+    function fadeSlide() {
+        setTimeout(() => {
+            $(".up").fadeToggle("fast");  
+            $(".up").fadeToggle("fast");  
+            $(".down").slideToggle("fast");   
+        }, 3000);
+        $(".down").slideToggle("fast");
     }
-
-    if (blockGame == false) {
-        const cards = document.querySelectorAll(".memory-card");
-        let firstCard, secondCard;
-        let lockCards = false;
-
-        function flipCard() {
-            if (lockCards) return false;
+ 
+    $("#btnStart").click(function() {
+        cardHTML = "";
+        board.innerHTML = "";
+        sortCards();
+        sortCards();
+        board.innerHTML = cardHTML;
+        game();
+        fadeSlide();
+    });
+    
+    function game() {
+        const cards = document.querySelectorAll(".cardGame");
+        let oneCard;
+        let twoCard;
+        let block = false;
+        let contGame = 0;
+        const contTime = performance.now();
+        
+        function match() {
+            let verify = oneCard.dataset.card === twoCard.dataset.card;
+            !verify ? myDown() : reset(verify);
+    
+            if (contGame == 8) {
+                alert(`O jogo acabou, seu tempo foi de ${(((performance.now() - contTime) % 60000) / 1000).toFixed(0)} segundos`);
+            }
+        }
+        
+        function myUp() {
+            if (block) return false;
             this.classList.add("flip");
-
-            if (!firstCard) {
-                firstCard = this;
+    
+            if (!oneCard) {
+                oneCard = this;
                 return false;
             }
+    
+            twoCard = this;
+            match();
+        }
+        
+        function myDown() {
+            block = true;
+    
+            setTimeout(() => {
+                oneCard.classList.remove("flip");
+                twoCard.classList.remove("flip");
+                reset();
+                alert("Você errou!");
+            }, 1500);
+        }
+        
+        function reset(verify) {
+            if (verify) {
+                oneCard.removeEventListener("click", myUp);
+                twoCard.removeEventListener("click", myUp);
+                contGame += 1;  
+            }
 
-            secondCard = this;
-
-            checkForMatch();
+            oneCard = null;
+            twoCard = null;
+            block = false;
         }
 
-        function checkForMatch() {
-        let isMatch = firstCard.dataset.card === secondCard.dataset.card;
-
-        !isMatch ? unFlipCards() : resetCards(isMatch);
-        }
-
-        function unFlipCards() {
-        lockCards = true;
-        setTimeout(() => {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-
-            resetCards();
-        }, 1000);
-        }
-
-        function resetCards(isMatch = false) {
-        if (isMatch) {
-            firstCard.removeEventListener("click", flipCard);
-            secondCard.removeEventListener("click", flipCard);
-        }
-
-        [firstCard, secondCard, lockCards] = [null, null, false];
-        }
-
-        cards.forEach(card => card.addEventListener("click", flipCard));    
-    }
-
-
-
-
-
+        cards.forEach(card => card.addEventListener("click", myUp));  
+    }  
 })();
 
 app.inicio();
